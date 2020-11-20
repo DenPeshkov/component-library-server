000825e87a69903a27431e1ecb06d4bf39fad6db
Peshkov Denis <peshkovdenis1985@gmail.com>
2020-11-20 17:58:45 +0300

    initial commit

---
+++ src/main/java/com/github/denpeshkov/componentlibraryserver/ComponentLibraryServerApplication.java
@@ -0,0 +1,12 @@
+package com.github.denpeshkov.componentlibraryserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComponentLibraryServerApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ComponentLibraryServerApplication.class, args);
+  }
+}
---
+++ src/test/java/com/github/denpeshkov/componentlibraryserver/ComponentLibraryServerApplicationTests.java
@@ -0,0 +1,11 @@
+package com.github.denpeshkov.componentlibraryserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComponentLibraryServerApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
